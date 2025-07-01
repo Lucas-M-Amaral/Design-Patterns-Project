3d19bf03121c0d17f320bc3810143aaa87e9cc89
viniCoio222 <viniciuus.santos@aluno.uece.br>
2025-06-30 21:09:44 -0300

    Setting MVC repository

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,40 @@
 import os
-from dotenv import load_dotenv
+import pymongo
 
-from os import getenv
+from typing_extensions import Annotated
+from pydantic import BeforeValidator
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-
-load_dotenv("./.env")
+# Database connection setup
+# ------------------------------------------------------------------------------
 DATABASE_URL = os.getenv("DATABASE_URL")
-print(DATABASE_URL)
+"""str: The URL for the MongoDB database connection."""
+MONGO_INITDB_DATABASE = os.getenv("MONGO_INITDB_DATABASE", "database")
+"""str: The name of the MongoDB database to connect to."""
+
+client = pymongo.AsyncMongoClient(DATABASE_URL)
+"""AsyncMongoClient: The MongoDB client for asynchronous operations."""
+db = client.get_database(MONGO_INITDB_DATABASE)
+"""Database: The MongoDB database instance."""
+
+
+async def get_db():
+    """Get the database connection."""
+    try:
+        yield db
+    finally:
+        # Close the client connection when done
+        await client.close()
+        print("Database connection closed.")
+
+# Type Aliases
+# ------------------------------------------------------------------------------
+PyObjectId = Annotated[str, BeforeValidator(str)]
+"""PyObjectId: A type alias for a string that represents a MongoDB ObjectId."""
+
+
+# Collections in the database.
+# ------------------------------------------------------------------------------
+collections = {
+    "users": db.get_collection("users"),
+}
+"""dict: A dictionary of collections in the database."""
